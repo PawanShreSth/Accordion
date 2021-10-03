64903751862f1c7d496c28acb6f933d7d0400e74
Pawan Shrestha <pawanshresthait@gmail.com>
2021-10-03 17:31:13 +0545

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,28 @@
+const questions = [
+  {
+    id: 1,
+    title: 'What is JavaScript?',
+    info: 'JavaScript is a client-side and server-side scripting language inserted into HTML pages and is understood by web browsers. JavaScript is also an Object-based Programming language',
+  },
+  {
+    id: 2,
+    title: 'What is the use of isNaN function?',
+    info: 'isNan function returns true if the argument is not a number; otherwise, it is false.',
+  },
+  {
+    id: 3,
+    title: 'Which company developed JavaScript?',
+    info: 'Netscape is the software company that developed JavaScript.',
+  },
+  {
+    id: 4,
+    title: 'What are global variables? How are these variable declared?',
+    info: 'Global variables are available throughout the length of the code so that it has no scope. The var keyword is used to declare a local variable or object. If the var keyword is omitted, a global variable is declared.',
+  },
+  {
+    id: 5,
+    title: 'What is ‘this’ keyword in JavaScript?',
+    info: '‘This’ keyword refers to the object from where it was called. it is dynamic.',
+  },
+];
+export default questions;
